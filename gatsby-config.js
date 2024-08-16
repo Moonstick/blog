6495c21f2a1057f29fd3290442daf0dd4a7b28a8
David O'Brien <d@vidobrien.com>
2024-08-16 09:49:25 +0100

    comment out config for sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,31 +6,36 @@ module.exports = {
     title: `David O'Brien : Personal Blog`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "blog",
-      "path": "./blog"
-    },
-    __key: "blog"
-  }]
+  plugins: [
+    "gatsby-plugin-image",
+    // "gatsby-plugin-sitemap", {
+    //   resolve: 'gatsby-plugin-manifest',
+    //   options: {
+    //     "icon": "src/images/icon.png"
+    //   }
+    // },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp", {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }, {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    }, {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "blog",
+        "path": "./blog"
+      },
+      __key: "blog"
+    }]
 };
