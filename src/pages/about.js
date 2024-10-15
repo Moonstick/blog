1bec3a512a60c370c3d1be2eb7f676dfc9ad776a
David O'Brien <d@vidobrien.com>
2024-10-15 16:55:32 +0100

    Update about and index page with better text

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,9 @@ import Layout from '../components/layout'
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>Hi, my name is David O'Brien and I'm working as a software consultant which generally involves working teams of people to design and build systems. I'm currently working for <a href="http://www.codurance.com">Codurance</a> as one of their Principal Craftsment.</p>
+      <p>As well as being interested in using software to solve problems, I'm also a bit of an amateur cook and some of that will probably be represented in the posts I make.</p>
+      <p>I've been promising that I'll blog more regularly for quite a long time so this is my effort to do that, mostly as a way of documenting some of the things I learn, as well as a way to make me more intentional about starting (and finishing) at least some of the projects I've been ruminating about the last decade or two.</p>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Seo from '../components/seo'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>David O'Brien's personal blog; views expressed here do not represent those of my employer, friends or family.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
         src="../images/clifford.jpg"
